e584c17e4fc04c29d58dbbf01f1ab1351e541fb2
oneofminyards <71725779+oneofminyards@users.noreply.github.com>
2020-10-01 20:55:59 +0300

    Initial commit

---
+++ Assets/Scripts/Step.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Step : MonoBehaviour
+{
+    [TextArea(15, 50)]
+    public string content;
+
+    public Step[] nextSteps;
+
+}
---
+++ Assets/Scripts/TextQuest.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TextQuest : MonoBehaviour
+{
+    public Text contentText;
+    public Step startStep;
+
+
+    Step currentStep;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentStep = startStep;
+     
+        contentText.text = startStep.content;
+
+        
+
+   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            currentStep = currentStep.nextSteps [0];
+            contentText.text = currentStep.content;
+        }
+        if(Input.GetKeyDown(KeyCode.Alpha2))
+        { currentStep = currentStep.nextSteps[1];
+            contentText.text = currentStep.content;
+        }
+        
+    }
+}
